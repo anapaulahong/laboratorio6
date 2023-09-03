01cb40520b6ed6a05faf1c125b1b50f1d525879d
anapaunavas <anapaulahong@gmail.com>
2023-09-03 13:16:56 -0500

    login

--- app/src/main/java/com/example/laboratory6/MainActivity.kt
+++ app/src/main/java/com/example/laboratory6/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.laboratory6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,7 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.laboratory6.ui.theme.Laboratory6Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,15 +43,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Login(name: String, modifier: Modifier = Modifier) {
-    val username by remember {
+    var username by remember {
          mutableStateOf("")
     }
-}
+    var password by remember {
+        mutableStateOf("")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Laboratory6Theme {
-        Login("Android")
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Username", modifier = Modifier.padding(start = 25.dp))
+        BasicTextField(
+            value = username,
+            onValueChange = { username = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp),
+            singleLine = true
+        )
+        Text("Password", modifier = Modifier.padding(start = 25.dp))
+        BasicTextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp),
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+
+        }) {
+            Text(text = "Login")
+        }
     }
 }
